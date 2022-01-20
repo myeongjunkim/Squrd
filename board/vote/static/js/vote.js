81cd548cf30baef9d929641e41e142e591ce76cb
caurental12 <caurental12@swmac12ui-MacBookPro.local>
2022-01-20 23:04:24 +0900

    refactor: vote.front

--- board/vote/static/js/vote.js
+++ board/vote/static/js/vote.js
@@ -9,3 +9,19 @@ function changeImg(event, backgroundImg) {
     // read the image file as a data URL.
     reader.readAsDataURL(event.target.files[0]);
 };
+
+function changeBorder(){
+    const input_1 = document.getElementById("contactChoice1")
+    const input_2 = document.getElementById("contactChoice2")
+    if (input_1.checked){
+        document.getElementById("label1").style.border="2px solid #041562"
+        document.getElementById("label2").style.border="0px solid #041562"
+    }
+    else if(input_2.checked){
+        document.getElementById("label2").style.border="2px solid #041562"
+        document.getElementById("label1").style.border="0px solid #041562"
+   
+    }
+}
+
+    
